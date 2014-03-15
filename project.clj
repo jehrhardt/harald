a1292f83cda516a40ee541539216492c01dd3bda
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2014-03-15 07:02:27 +0100

    Add noderepl to project

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173" :scope "provided"]])
+                 [org.clojure/clojurescript "0.0-2173" :scope "provided"]]
+
+  :plugins [[org.bodil/lein-noderepl "0.1.11"]])
