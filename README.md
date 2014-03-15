509d052ae95f4aea4313a57a42c53c4269f0063e
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2014-03-15 06:53:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+harald
+======
+
+ClojureScript library for Bluetooth Low Energy.
+
+License
+-------
+
+Copyright © 2014 [Jan Ehrhardt](http://derjan.io).
+
+Distributed under the
+[Eclipse Public License](http://www.eclipse.org/legal/epl-v10.html).
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject harald "0.1.0-SNAPSHOT"
+  :description "ClojureScript library for Bluetooth Low Energy"
+  :url "https://github.com/jehrhardt/harald"
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173" :scope "provided"]])
