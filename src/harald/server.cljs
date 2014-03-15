9b241cff0aad1ab0958a3dae7d09da3179800023
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2014-03-15 20:56:28 +0100

    Add restify hello world sample

--- src/harald/server.cljs
+++ src/harald/server.cljs
@@ -1,11 +1,21 @@
 (ns harald.server
   (:require [cljs.nodejs :as nodejs]))
 
+(def restify (nodejs/require "restify"))
+
 (defn greeting []
   (str "Hello " "from " "harald!"))
 
+(defn respond [request, response, next]
+  (.send response (greeting)))
+
+(defn run-server [port]
+  (let [server (.createServer restify)]
+    (.get server "/" respond)
+    (.listen server port #(println (str "Harald is running on port " port)))))
+
 (defn -main [& args]
-  (println (greeting)))
+  (println (run-server 8080)))
 
 (nodejs/enable-util-print!)
 (set! *main-cli-fn* -main)
