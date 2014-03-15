275dba53b23ecc7a74e0c198df9cb5cf8ed2d04c
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2014-03-15 08:42:01 +0100

    Move ClojureScript plugins to dev profile
    The plugins for ClojureScript development are only used in the
    examples. They are not required for distributing the library.

--- project.clj
+++ project.clj
@@ -8,11 +8,10 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2173" :scope "provided"]]
 
-  :plugins [[org.bodil/lein-noderepl "0.1.11"]
-            [lein-npm "0.3.0"]
-            [lein-cljsbuild "1.0.2"]]
-
-  :profiles {:dev {:node-dependencies [[bleno "0.1.3"]]}}
+  :profiles {:dev {:node-dependencies [[bleno "0.1.3"]]
+                   :plugins [[org.bodil/lein-noderepl "0.1.11"]
+                             [lein-npm "0.3.0"]
+                             [lein-cljsbuild "1.0.2"]]}}
 
   :cljsbuild {:builds [{:id "sensortag"
                         :source-paths ["src" "examples/sensortag/src"]
